5a2dda4ee425017fa6d0308e88530ff1810a7abc
badogan <basri.dogan.71@gmail.com>
2020-04-24 22:27:22 +0100

    Header and signin mockup in pace.

--- src/App.js
+++ src/App.js
@@ -1,64 +1,14 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { maxHeight } from "@material-ui/system";
-// import "./App.css";
-// import Button from "./components/button/button";
-// import Hello from "./components/hello/hello";
-// import ButtonMUI from "./components/button-MUI/button-MUI";
-// import Hook from './components/loginForm/loginForm'
-
-const useStyles = makeStyles(theme => ({
-  paperStyle: {
-    height: maxHeight,
-    // backgroundColor: theme.palette.grey[800],
-    color: theme.palette.common.white,
-    marginBottom: theme.spacing(4),
-    // backgroundImage: "url(https://source.unsplash.com/random)",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center"
-  },
-  header: {
-    marginTop: 100
-  }
-}));
+import SigninSide from "./pages/SigninSide";
+import Header from "./pages/Header";
 
 function App() {
-  const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
-      <Paper height="100%" className={classes.paperStyle} elevation={3}>
-        <Typography
-          className={classes.header}
-          color="primary"
-          gutterBottom="true"
-          variant="h3"
-          align="center"
-        >
-          I WANT MY BOOK BACK
-        </Typography>
-        <Grid container direction="row" justify="left" alignItems="center">
-          <Typography color="secondary" variant="h5" align="left">
-            Keep track of your books!
-          </Typography>
-          <Typography color="secondary" variant="h5" align="center">
-            dnm 2
-          </Typography>
-        </Grid>
-      </Paper>
-      {/* <div className="App"> */}
-      {/* <header className="App-header"> */}
-      {/* <Button label='click me please'/> */}
-      {/* <ButtonMUI /> */}
-      {/* <Hello /> */}
-      {/* <Hook/> */}
-      {/* </header> */}
-      {/* </div> */}
+      <Header/>
+      <SigninSide />
     </React.Fragment>
   );
 }
---
+++ src/pages/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import LocalLibraryOutlinedIcon from '@material-ui/icons/LocalLibraryOutlined';
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <LocalLibraryOutlinedIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+              I Want My Book Back!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+}
